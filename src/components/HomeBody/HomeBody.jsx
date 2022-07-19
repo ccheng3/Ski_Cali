1eb236541fcfa4278ff5ff72756ebfb35b846d6b
ccheng3 <ccrcheng@ucdavis.edu>
2022-07-18 18:06:25 -0700

    added some responsiveness to home page title

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 
 import { Box, Typography } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import background from '../../images/hero_banner.jpg';
 
+const theme = createTheme();
+
+theme.typography.h3 = {
+   fontSize: '1.2rem',
+   '@media (min-width:0px)': {
+      fontSize: '1.5rem',
+      lineHeight: '3.2rem',
+      // backgroundPosition: '90% 70%',
+   },
+   [theme.breakpoints.up('md')]: {
+      fontSize: '3.6rem',
+      lineHeight: '4.8rem',
+   },
+};
+
 export default function HomeBody() {
+
    return (
       <Box style={{
          display: 'flex',
@@ -15,14 +32,16 @@ export default function HomeBody() {
          width: '100%',
          height: '100vh',
       }}>
-         <Typography variant='h2' style={{
-            padding: '3.2rem 3.6rem',
-            color: 'rgb(230,230,230)',
-            textTransform: 'uppercase',
-            fontWeight: 'bold',
-         }}>
-            Almost 40,000 total acres of cold blower to shred. Will you answer the call?
-         </Typography>
+         <ThemeProvider theme={theme}>
+            <Typography variant="h3" style={{
+               padding: '3.2rem 3.6rem',
+               color: 'rgb(230,230,230)',
+               textTransform: 'uppercase',
+               fontWeight: 'bold',
+            }}>
+               Almost 40,000 total acres of cold blower to shred. Will you answer the call?
+            </Typography>
+         </ThemeProvider>
       </Box>
    )
 };
