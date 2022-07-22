8b09f14339400416d43087d8ff271d05499ff53c
ccheng3 <ccrcheng@ucdavis.edu>
2022-07-22 00:10:46 -0700

    still not sure wtf overlay transition is is not working due to, prolly CSS brainfart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Toolbar, Typography, Button } from '@mui/material';
+import { Toolbar, Stack } from '@mui/material';
 import { NavLink } from 'react-router-dom';
 
 import logo from '../../images/skiing.png';
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 import './Header.css'
 
 export default function Header() {
+   const [toggleMenu, setToggleMenu] = React.useState(false);
+
    const handleLinkStyle = ({ isActive }) => {
       return {
          textDecoration: isActive ? 'none' : 'underline',
@@ -14,19 +18,36 @@ export default function Header() {
    }
 
    return (
-      <Toolbar className='main-nav'>
-         <NavLink className='main-navlink' to='/'>
-            <img className='logo'
+      <Toolbar className='header-nav'>
+         <NavLink className='header-nav-link' to='/'>
+            <img className='header-logo'
                src={logo}
             />
          </NavLink>
-         <span className='title'>Ski California</span>
-         <span>
-            <NavLink style={handleLinkStyle} className='main-navlink' to='/about'>About</NavLink>
-            <NavLink style={handleLinkStyle} className='main-navlink' to='/resorts'>Resorts</NavLink>
-            <NavLink style={handleLinkStyle} className='main-navlink' to='/traffic'>Highway Conditions</NavLink>
-            <NavLink style={handleLinkStyle} className='main-navlink' to='/safety'>Snow Safety </NavLink>
+         <span className='header-title'>Ski California</span>
+         <span className='header-nav-span'>
+            <NavLink style={handleLinkStyle} className='header-nav-link' to='/about'>About</NavLink>
+            <NavLink style={handleLinkStyle} className='header-nav-link' to='/resorts'>Resorts</NavLink>
+            <NavLink style={handleLinkStyle} className='header-nav-link' to='/traffic'>Highway Conditions</NavLink>
+            <NavLink style={handleLinkStyle} className='header-nav-link' to='/safety'>Snow Safety </NavLink>
          </span>
+
+         <div className='header-small-screen-menu'>
+            <MenuIcon onClick={() => { setToggleMenu(true) }} />
+            {toggleMenu &&
+               <div className='header-small-screen-overlay'>
+                  <CloseIcon className='header-small-screen-overlay-close' onClick={() => { setToggleMenu(false) }} />
+                  <Stack spacing={2}
+                     justifyContent="center"
+                     alignItems="center"
+                     className='header-small-screen-overlay-stack'>
+                     <NavLink style={handleLinkStyle} className='header-nav-link' onClick={() => { setToggleMenu(false) }} to='/about'>About</NavLink>
+                     <NavLink style={handleLinkStyle} className='header-nav-link' onClick={() => { setToggleMenu(false) }} to='/resorts'>Resorts</NavLink>
+                     <NavLink style={handleLinkStyle} className='header-nav-link' onClick={() => { setToggleMenu(false) }} to='/traffic'>Highway Conditions</NavLink>
+                     <NavLink style={handleLinkStyle} className='header-nav-link' onClick={() => { setToggleMenu(false) }} to='/safety'>Snow Safety </NavLink>
+                  </Stack>
+               </div>}
+         </div>
       </Toolbar >
 
    );
