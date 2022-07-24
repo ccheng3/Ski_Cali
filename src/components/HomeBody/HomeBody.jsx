62e9bf702b50483d808015b533c0b69e16db621a
ccheng3 <ccrcheng@ucdavis.edu>
2022-07-24 11:33:45 -0700

    added view resort conditions and check traffic buttons to home page to provide call to action

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, Stack } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { NavLink } from 'react-router-dom';
 
 import background from '../../images/hero_banner.jpg';
+import CustomButton from '../CustomButton/CustomButton';
 
 const theme = createTheme();
 
@@ -31,16 +33,57 @@ export default function HomeBody() {
          width: '100%',
          height: '100vh',
       }}>
-         <ThemeProvider theme={theme}>
-            <Typography variant="h3" style={{
-               padding: '3.2rem 3.6rem',
-               color: 'rgb(230,230,230)',
-               textTransform: 'uppercase',
-               fontWeight: 'bold',
-            }}>
-               Almost 40,000 total acres of cold blower to shred. Will you answer the call?
-            </Typography>
-         </ThemeProvider>
+         <Stack sx={{
+            display: 'flex',
+            alignItems: 'center',
+         }}>
+            <ThemeProvider theme={theme}>
+               <Typography variant="h3" style={{
+                  padding: '1.8rem 3.6rem',
+                  color: 'rgb(230,230,230)',
+                  textTransform: 'uppercase',
+                  fontWeight: 'bold',
+               }}>
+                  Almost 40,000 total acres of cold blower to shred. Will you answer the call?
+               </Typography>
+            </ThemeProvider>
+            <Stack direction='row'
+               sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  padding: { xs: '0rem 2.4rem', sm: 0 },
+               }}
+            >
+               <NavLink to='/resorts'>
+                  <CustomButton sx={{
+                     marginRight: '1.5rem',
+                     color: '#2d4850',
+                     backgroundColor: '#ed6c02',
+                     '&:hover': {
+                        backgroundColor: '#fff',
+                        color: '#3c52b2',
+                     },
+                  }}
+                     variant='contained'>
+                     View Resort Conditions
+                  </CustomButton>
+               </NavLink>
+               <NavLink to='/traffic'>
+                  <CustomButton sx={{
+                     marginLeft: '1.5rem',
+                     border: '1px solid yellow',
+                     color: 'yellow',
+                     '&:hover': {
+                        backgroundColor: '#fff',
+                        color: '#2d4850',
+                     },
+                  }}
+                     variant='outlined'>
+                     Check Traffic
+                  </CustomButton>
+               </NavLink>
+            </Stack>
+         </Stack>
       </Box>
    )
 };
